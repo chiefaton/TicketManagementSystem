96f39e506df110852e0286b8ca4996958a684441
Cheifaton <115976568+chiefaton@users.noreply.github.com>
2023-07-03 00:46:57 +0530

    Update Default.aspx.vb
    update & delete

--- TicketManagementSystem/Default.aspx.vb
+++ TicketManagementSystem/Default.aspx.vb
@@ -51,6 +51,63 @@ Public Class _Default
 
     End Sub
 
+
+    Protected Sub btnUpdate_Click(sender As Object, e As EventArgs) Handles btnUpdate.Click
+    ' Get the ticket ID from the hidden field.
+    Dim ticketID As Integer = Convert.ToInt32(ViewState("ticketID"))
+
+    ' Get the new ticket information from the form controls.
+    Dim title As String = txtTitle.Text
+    Dim description As String = txtDescription.Text
+    Dim severity As String = RadioButtonsSeverity.SelectedItem.Text
+    Dim priority As String = DropDownListPriority.SelectedItem.Text
+    Dim email As String = txtEmail.Text
+    Dim raisedDate As Date = CalendarDate.SelectedDate
+
+    ' Update the ticket in the database.
+    Dim connection As New SqlConnection("Data Source=(LocalDB)\MSSQLLocalDB;Initial Catalog=TicketManagementSystem;Integrated Security=True")
+    connection.Open()
+
+    Dim command As New SqlCommand("UPDATE TicketInfo SET Title=@Title, Description=@Description, Severity=@Severity, Priority=@Priority, Email=@Email, RaisedDate=@RaisedDate WHERE TicketID=@TicketID", connection)
+    command.Parameters.AddWithValue("@Title", title)
+    command.Parameters.AddWithValue("@Description", description)
+    command.Parameters.AddWithValue("@Severity", severity)
+    command.Parameters.AddWithValue("@Priority", priority)
+    command.Parameters.AddWithValue("@Email", email)
+    command.Parameters.AddWithValue("@RaisedDate", raisedDate)
+    command.Parameters.AddWithValue("@TicketID", ticketID)
+
+    command.ExecuteNonQuery()
+
+    connection.Close()
+
+    ' Redirect the user to the home page.
+    Response.Redirect("~/")
+End Sub
+
+Protected Sub btnDelete_Click(sender As Object, e As EventArgs) Handles btnDelete.Click
+    Dim ticketID As Integer = Convert.ToInt32(ViewState("ticketID"))
+    Dim connection As New SqlConnection("Data Source=(LocalDB)\MSSQLLocalDB;Initial Catalog=TicketManagementSystem;Integrated Security=True")
+    connection.Open()
+    Dim command As New SqlCommand("DELETE FROM TicketInfo WHERE TicketID=@TicketID", connection)
+    command.Parameters.AddWithValue("@TicketID", ticketID)
+    command.ExecuteNonQuery()
+    connection.Close()
+    Response.Redirect("~/")
+End Sub
+
+    Protected Sub btnDelete_Click(sender As Object, e As EventArgs) Handles btnDelete.Click
+    Dim ticketID As Integer = Convert.ToInt32(ViewState("ticketID"))
+    Dim connection As New SqlConnection("Data Source=(LocalDB)\MSSQLLocalDB;Initial Catalog=TicketManagementSystem;Integrated Security=True")
+    connection.Open()
+    Dim command As New SqlCommand("DELETE FROM TicketInfo WHERE TicketID=@TicketID", connection)
+    command.Parameters.AddWithValue("@TicketID", ticketID)
+    command.ExecuteNonQuery()
+    connection.Close()
+    Response.Redirect("~/")
+End Sub
+
+    
     Private Sub ListProduct()
         Dim command As New SqlCommand("select * from TicketInfo", connect)
         Dim sd As New SqlDataAdapter(command)
